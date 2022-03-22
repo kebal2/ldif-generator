2ea7317ffeb833a98a5db8144350971ae420f823
Kelemen Bálint <kebal84@gmail.com>
2022-03-22 12:28:50 +0100

    feat: ldif file generation WIP

---
+++ App/Program.cs
@@ -0,0 +1,19 @@
+﻿using ldifgen;
+
+var g = new Generator();
+
+var userObjectClasses = new[]
+{
+    "top",
+    "person",
+    "organizationalPerson",
+    "inetOrgPerson"
+};
+
+var orgUnits = new[] { "Account", "Home", "Apple" };
+
+var groups = new[] { "" };
+
+var lDif = g.GenLDif("dc=example,dc=com", true, "RootDomain", orgUnits, "add", 10, userObjectClasses, Generator.cbType.GENERIC);
+
+File.WriteAllText("output.ldif", lDif);
