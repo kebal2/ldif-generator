c5398348ae4739f2d64e5284bd41cb2f778f1828
Kelemen Bálint <kebal84@gmail.com>
2022-03-26 14:55:12 +0100

    feat: console parameters for configuration

---
+++ ConsoleApp/Options.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+using CommandLine;
+
+using LdapEntityGenerator.Entities;
+
+namespace App;
+
+public class Options
+{
+    [Option('u', "users", Required = true, HelpText = "Set generated user count.")]
+    public int UserCount { get; set; }
+
+    [Option('g', "groups", Required = true, HelpText = "Set generated group count.")]
+    public int GroupCount { get; set; }
+
+    [Option('o', "ous", Required = true, HelpText = "Set generated organization unit count.")]
+    public int OuCount { get; set; }
+
+    [Option('p', "password", Required = false, HelpText = "Set user password.", Default = "AnExamplePassword1!")]
+    public string Password { get; set; }
+
+    [Option('a', "createAdmin", Required = false, HelpText = "Should create admin user.", Default = true)]
+    public bool CreateAdmin { get; set; }
+
+    [Option('e', "createBaseOu", Required = false, HelpText = "Should create base organization unit.", Default = true)]
+    public bool CreateBaseOu { get; set; }
+
+    [Option('b', "baseDomain", Required = false, HelpText = "Base domain name.", Default = "example.org")]
+    public string BaseDomain { get; set; }
+
+    [Option('y', "createRootOu", Required = false, HelpText = "Should create root organization unit.", Default = true)]
+    public bool CreateRootOu { get; set; }
+
+    [Option('r', "rootOu", Required = false, HelpText = "Should create root organization unit.", Default = "RootOU")]
+    public string RootOu { get; set; }
+
+    [Option('t', "type", Required = false, HelpText = "Generated ldif file system type [MSAD, generic].", Default = CbType.MAD)]
+    public CbType LdifType { get; set; }
+
+    [Option('c', "accessControl", Required = false, HelpText = "Set MSAD user access control.", Default = 0x0200)]
+    public int UserAccessControl { get; set; }
+
+    [Option('s', "fileSizeLimit", Required = false, HelpText = "Set generated file size limit.", Default = 2_000_000)]
+    public int FileSizeLimit { get; set; }
+    
+    [Option('x', "outputPath", Required = false, HelpText = "Set output file path.", Default = ".")]
+    public string OutputPath { get; set; }
+    
+    [Option('f', "fileName", Required = false, HelpText = "Set output file path.", Default = "output.ldif")]
+    public string FileName { get; set; }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,33 +1,54 @@
 ﻿using System.Reflection;
 
+using App;
+
+using CommandLine;
+
 using LdapEntityGenerator;
 using LdapEntityGenerator.Entities;
 
+
+Options? o = default;
+
+Parser
+    .Default
+    .ParseArguments<Options>(args)
+    .WithParsed(opt => o = opt);
+
 var g = new EntityGenerator();
 
-var orgUnits = new[] { "Account", "Home", "Apple" };
-
-var groups = new[] { "" };
-
-var p = new EntityGenerator.LdapEntryOptions("example.com",  "RootDomain",  10)
+var p = new EntityGenerator.LdapEntryOptions(o.BaseDomain,  o.RootOu,  o.UserCount)
 {
-    OrgUnits = orgUnits,
-    CreateAdmin = true,
-    Password = "Password1",
-    CreateBaseOrganization = true,
-    CreateRootOu = true
+    OrgUnits = Enumerable.Range(1, o.OuCount).Select(i => $"organizationUnit{i:0000}").ToArray(),
+    Groups = Enumerable.Range(1, o.GroupCount).Select(i => $"group{i:0000}").ToArray(),
+    
+    CreateAdmin = o.CreateAdmin,
+    Password = @$"""{o.Password}""",
+    CreateBaseOrganization = o.CreateBaseOu,
+    CreateRootOu = o.CreateRootOu,
+    CbType = o.LdifType,
+    UserAccountControl = (UserAccessControlFlags)o.UserAccessControl,
 };
 
 var lDif = g.GetLdapEntries(p, Console.Out);
 
 LdifFileRenderer renderer = new LdifFileRenderer();
 
-var ldifChunks = renderer.RenderDiff(lDif, 5000);
+var ldifChunks = renderer.RenderDiff(lDif, o.FileSizeLimit);
+
+var filePath = o.OutputPath == "." ? (new FileInfo(Assembly.GetExecutingAssembly().Location)).Directory.FullName: o.OutputPath;
+
+var fileParts = o.FileName.Split(".").ToList();
+fileParts.Insert(fileParts.Count - 1, "{0}");
+var fileFormat = string.Join(".", fileParts);
 
 for (var i = 0; i < ldifChunks.Length; i++)
 {
-    var fileName = $"output{i}.ldif";
-    File.WriteAllText(fileName, ldifChunks[i]);
+    var fileName = string.Format(fileFormat,i);
+
+    var path = Path.Combine(filePath, fileName);
+        
+    File.WriteAllText(path, ldifChunks[i]);
     
-    Console.Out.WriteLine(Path.Combine((new FileInfo(Assembly.GetExecutingAssembly().Location)).Directory.FullName, fileName));
+    Console.Out.WriteLine(path);
 }
