0e922d9bb889d7d131e8ac3b8fe22d02fd4d2473
Kelemen Bálint <kebal84@gmail.com>
2022-03-26 14:03:29 +0100

    add: userAccountControl flag enum and base64 tools

---
+++ LdapEntityGenerator/Tools.cs
@@ -0,0 +1,13 @@
+﻿namespace LdapEntityGenerator;
+
+public static class Tools
+{
+    public static string AsBase64(this string plainText) {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+        return System.Convert.ToBase64String(plainTextBytes);
+    }
+    
+    public static string DecodeBase64(this byte[] base64EncodedData) {
+        return System.Text.Encoding.UTF8.GetString(base64EncodedData);
+    }
+}
