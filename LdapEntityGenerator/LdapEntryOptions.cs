1d676980af66dfc6c68824c0c394ade5eed21d62
Deák Tamás <dk.tamas@gmail.com>
2022-03-28 10:56:57 +0200

    LdapEntryOptions in own file

---
+++ LdapEntityGenerator/LdapEntryOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace LdapEntityGenerator;
+
+public class LdapEntryOptions
+{
+    public LdapEntryOptions(string baseDomain, string rootOu, int userCount)
+    {
+        BaseDomain = baseDomain;
+        RootOu = rootOu;
+        UserCount = userCount;
+    }
+
+    public string BaseDomain { get; set; }
+    public string RootOu { get; }
+    public int UserCount { get; }
+    public string[] OrgUnits { get; set; }
+    public string ChangeType { get; set; } = "add";
+    public bool CreateAdmin { get; set; }
+    public string Password { get; set; }
+    public bool CreateBaseOrganization { get; set; }
+    public bool CreateRootOu { get; set; }
+    public string[] Groups { get; set; }
+    public UserAccessControlFlags UserAccountControl { get; set; }
+}
