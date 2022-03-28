af8bfa7c38ce0a573557c08638b4c1728f495c44
Deák Tamás <dk.tamas@gmail.com>
2022-03-28 10:59:20 +0200

    switching on LdifType in Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,7 +5,6 @@ using App;
 using CommandLine;
 
 using LdapEntityGenerator;
-using LdapEntityGenerator.Entities;
 
 Options? o = default;
 
@@ -14,40 +13,56 @@ Parser
     .ParseArguments<Options>(args)
     .WithParsed(opt => o = opt);
 
-var g = new EntityGenerator();
-
-var p = new EntityGenerator.LdapEntryOptions(o.BaseDomain, o.RootOu, o.UserCount)
+if (o is null)
+    Console.ReadKey();
+else
 {
-    OrgUnits = Enumerable.Range(1, o.OuCount).Select(i => $"organizationUnit{i:0000}").ToArray(),
-    Groups = Enumerable.Range(1, o.GroupCount).Select(i => $"group{i:0000}").ToArray(),
+    var p = new LdapEntryOptions(o.BaseDomain, o.RootOu, o.UserCount)
+    {
+        OrgUnits = Enumerable.Range(1, o.OuCount).Select(i => $"organizationUnit{i:0000}").ToArray(),
+        Groups = Enumerable.Range(1, o.GroupCount).Select(i => $"group{i:0000}").ToArray(),
 
-    CreateAdmin = o.CreateAdmin,
-    Password = @$"""{o.Password}""",
-    CreateBaseOrganization = o.CreateBaseOu,
-    CreateRootOu = o.CreateRootOu,
-    CbType = o.LdifType,
-    UserAccountControl = (UserAccessControlFlags)o.UserAccessControl,
-};
+        CreateAdmin = o.CreateAdmin,
+        Password = @$"""{o.Password}""",
+        CreateBaseOrganization = o.CreateBaseOu,
+        CreateRootOu = o.CreateRootOu,
+        UserAccountControl = (UserAccessControlFlags)o.UserAccessControl,
+    };
 
-var lDif = g.GetLdapEntries(p, Console.Out);
+    EntityGenerator? g = default;
 
-LdifFileRenderer renderer = new LdifFileRenderer();
+    switch (o.LdifType)
+    {
+        case LdapEntityGenerator.Entities.CbType.MAD:
+            g = new Mad();
+            break;
+        case LdapEntityGenerator.Entities.CbType.GENERIC:
+            g = new Generic();
+            break;
+        default:
+            throw new Exception($"Unknown {nameof(o.LdifType)}: {o.LdifType}");
+    }
 
-var ldifChunks = renderer.RenderDiff(lDif, o.FileSizeLimit);
+    var lDif = g.GetLdapEntries(p, Console.Out);
 
-var filePath = o.OutputPath == "." ? (new FileInfo(Assembly.GetExecutingAssembly().Location)).Directory.FullName : o.OutputPath;
+    LdifFileRenderer renderer = new LdifFileRenderer();
 
-var fileParts = o.FileName.Split(".").ToList();
-fileParts.Insert(fileParts.Count - 1, "{0}");
-var fileFormat = string.Join(".", fileParts);
+    var ldifChunks = renderer.RenderDiff(lDif, o.FileSizeLimit);
 
-for (var i = 0; i < ldifChunks.Length; i++)
-{
-    var fileName = string.Format(fileFormat, i);
+    var filePath = o.OutputPath == "." ? (new FileInfo(Assembly.GetExecutingAssembly().Location)).Directory.FullName : o.OutputPath;
 
-    var path = Path.Combine(filePath, fileName);
+    var fileParts = o.FileName.Split(".").ToList();
+    fileParts.Insert(fileParts.Count - 1, "{0}");
+    var fileFormat = string.Join(".", fileParts);
 
-    File.WriteAllText(path, ldifChunks[i]);
+    for (var i = 0; i < ldifChunks.Length; i++)
+    {
+        var fileName = string.Format(fileFormat, i);
 
-    Console.Out.WriteLine(path);
+        var path = Path.Combine(filePath, fileName);
+
+        File.WriteAllText(path, ldifChunks[i]);
+
+        Console.Out.WriteLine(path);
+    }
 }
