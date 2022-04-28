569422c25172fa3b0f16d74b212610945b401eb2
Kelemen Bálint <kebal84@gmail.com>
2022-04-28 18:40:30 +0200

    add: more Element factory

--- ConsoleApp/ElemFactory.cs
+++ ConsoleApp/ElemFactory.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-
-using Microsoft.VisualBasic.FileIO;
+﻿using NStack;
 
 using Terminal.Gui;
 
@@ -8,21 +6,7 @@ namespace App;
 
 internal static class ElemFactory
 {
-    const int DefaultWidth = 40;
-
-    internal static (Label label, TextField field) CreateTextField(string labelText, View relativeTo, string defaultValue = "", int width = DefaultWidth, int xOffset = 0, int yOffset = 0)
-    {
-        var yPos = Pos.Bottom(relativeTo) + yOffset;
-        var label = new Label(labelText) { X = Pos.Left(relativeTo) + xOffset, Y = yPos };
-        var field = new TextField(defaultValue)
-        {
-            X = Pos.Right(label),
-            Y = yPos,
-            Width = width
-        };
-
-        return (label, field);
-    }
+    private const int DefaultWidth = 40;
 
     internal static (Label label, TextField field) CreateTextField(string labelText, Pos x, Pos y, string defaultValue = "", int width = DefaultWidth)
     {
@@ -32,14 +16,71 @@ internal static class ElemFactory
             X = Pos.Right(label),
             Y = Pos.Top(label),
             Width = width,
+            Id = labelText
         };
 
         return (label, field);
     }
 
-    internal static CheckBox CreateCheckBox(string title, Pos x, Pos y, bool defaultValue = false) =>
-        new CheckBox(title) { X = x, Y = y, Checked = defaultValue };
+    internal static (Label label, TextField field) CreateTextField(string labelText, View relativeTo, string defaultValue = "", int width = DefaultWidth, int xOffset = 0, int yOffset = 0)
+    {
+        return CreateTextField(labelText, Pos.Left(relativeTo) + xOffset, Pos.Bottom(relativeTo) + yOffset, defaultValue, width);
+    }
 
-    internal static CheckBox CreateCheckBox(string title, View reference, int xOffset = 0, int yOffset = 0, bool defaultValue = false) =>
-        new CheckBox(title) { X = Pos.Left(reference) + xOffset, Y = Pos.Bottom(reference) + yOffset, Checked = defaultValue };
+    internal static CheckBox CreateCheckBox(string title, Pos x, Pos y, bool defaultValue = false)
+    {
+        return new(title) { X = x, Y = y, Checked = defaultValue, Id = title };
+    }
+
+    internal static CheckBox CreateCheckBox(string title, View reference, int xOffset = 0, int yOffset = 0, bool defaultValue = false)
+    {
+        return CreateCheckBox(title, Pos.Left(reference) + xOffset, Pos.Bottom(reference) + yOffset, defaultValue);
+    }
+
+    internal static (FrameView frame, RadioGroup field) CreateFramedRadioGroup(string title, string[] values, Pos x, Pos y, int defaultValue = 0)
+    {
+        var frame = new FrameView((string?)title)
+        {
+            X = x,
+            Y = y,
+            Width = title.Length + 6, Height = 4
+        };
+
+        var radioGroup = new RadioGroup(values.Select(ustring.Make).ToArray(), defaultValue);
+
+        frame.Add(radioGroup);
+
+        return (frame, radioGroup);
+    }
+
+    internal static (FrameView frame, RadioGroup field) CreateFramedRadioGroup(string title, string[] values, View reference, int xOffset = 0, int yOffset = 0, int defaultValue = 0)
+    {
+        var xPos = Pos.Left(reference) + xOffset;
+        var yPos = Pos.Bottom(reference) + 1 + yOffset;
+
+        return CreateFramedRadioGroup(title, values, xPos, yPos, defaultValue);
+    }
+
+    internal static RadioGroup CreateRadioGroup(string title, string[] values, Pos x, Pos y, int defaultValue = 0)
+    {
+        return new(values.Select(ustring.Make).ToArray(), defaultValue) { X = x, Y = y };
+    }
+
+    internal static RadioGroup CreateRadioGroup(string title, string[] values, View reference, int xOffset = 0, int yOffset = 0, int defaultValue = 0)
+    {
+        var xPos = Pos.Left(reference) + xOffset;
+        var yPos = Pos.Bottom(reference) + 1 + yOffset;
+
+        return CreateRadioGroup(title, values, xPos, yPos, defaultValue);
+    }
+
+    public static Button CreateButton(string title, Pos x, Pos y)
+    {
+        return new(title) { X = x, Y = y };
+    }
+
+    public static Button CreateButton(string title, View referenceField, int offsetX = 0, int offsetY = 0)
+    {
+        return CreateButton(title, Pos.Left(referenceField) + offsetX, Pos.Bottom(referenceField) + offsetY);
+    }
 }
